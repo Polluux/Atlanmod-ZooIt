009acb191907b5ca1a4018618b0bec46763eb8dc
Florent ALAPETITE <florent037@gmail.com>
2018-11-16 14:18:28 +0100

    Added the artifact generation from the maven archetype

--- services/create_artifact.js
+++ services/create_artifact.js
@@ -1,40 +1,43 @@
 const fs = require('fs')
 const path = require('path')
 const zipFolder = require('zip-folder')
-const spawn = require('child_process');
 
 
 function createFolder(dirPath){
 	try{
 		fs.mkdirSync(path.join(__dirname,"../services/",dirPath))
-	}catch(err){
-		if (err.code !== "EEXIST") throw err
+	}catch(err){		
+		//if (err.code !== "EEXIST") throw err	
+		console.log("OOpsi doopsi ça exist déjà")	
 	}
 }
 
-function generateMavenArtifact(artifactID, groupID, version){	
+function generateMavenArtifact(artifactID, groupID, version, callback){	
 	// Generate the Maven artifact with the 
-	try{			
-		var cd = spawn.spawn('cd', [artifactID]);
-		var yes = spawn.spawn('yes');
-		var mvn = spawn.spawn('mvn', ["archetype:generate", "-DarchetypeCatalog=local", "-DarchetypeGroupId=com.atlanmod.zoo", "-DarchetypeArtifactId=xcore-generation-archetype", "-DarchetypeVersion=1.0", "-DgroupId="+groupID, "-DartifactId="+artifactID, "-Dversion="+version]);
 
-		yes.stdout.pipe(mvn.stdin);	
+	var whitelist_regex = /^([A-Z]|[a-z]|[0-9]|\.|\-)+$/;
 
-		cd.on('error', function(err) {
-			console.log(err);
+	if (!whitelist_regex.test(artifactID))
+		callback("Error : artifactID is invalid");
+	else if (!whitelist_regex.test(groupID)) 
+		callback("Error : groupID is invalid");
+	else if (!whitelist_regex.test(version))
+		callback("Error : version is invalid");
+	else{
+
+		var command = "yes |mvn archetype:generate -DarchetypeCatalog=local -DarchetypeGroupId=com.atlanmod.zoo -DarchetypeArtifactId=xcore-generation-archetype -DarchetypeVersion=1.0 -DgroupId="+groupID+" -DartifactId="+artifactID+" -Dversion="+version;
+		
+		const execSync = require('child_process').execSync;
+
+		execSync(command, {cwd: path.join(__dirname,"../services/",artifactID)}, (e, stdout, stderr)=> {
+			if (e instanceof Error) {		
+				console.error(e);	
+			}		
+			console.log('stdout ', stdout);		
+			console.log('stderr ', stderr);
 		});
 
-		yes.on('error', function(err) {
-			console.log(err);
-		});
-
-		mvn.on('error', function(err) {
-			console.log(err);
-		});
-	}
-	catch (err){
-		throw err;
+		callback(null);
 	}
 
 }
@@ -54,28 +57,35 @@ function zipArchitecture(dirPath, callback){
 
 function createArtifact(propertiesObject,callback){
 	//create tmp folder with propertiesObject.artefact_name
+
+	console.log(propertiesObject)
 	createFolder("./"+propertiesObject.artifactID)
-	generateMavenArtifact(propertiesObject.artifactID, propertiesObject.groupID, propertiesObject.version);
-
-	
-	//Copy the xcore file from the tmp directory to services
-	//Ask Roxane about the #, her idea
-	var ouch = propertiesObject.artifactID+"/"+propertiesObject.artefact_name+"/src/main/model/"
-	fs.createReadStream(propertiesObject.file.split('#')[0]).pipe(fs.createWriteStream(path.join(__dirname,"../services/",ouch+propertiesObject.file.split('#')[1])));
-
-
-	//zip the artifact
-	zipArchitecture("./"+propertiesObject.artifactID, function(err,res){
+	generateMavenArtifact(propertiesObject.artifactID, propertiesObject.groupID, propertiesObject.version, function(err){
+		
 		if(err){
-			console.log(err); //Maybe do something else there (throw ?)
+			callback(err,null)
 		}else{
-			//return the artifact
-			callback(err,res)//the .zip file
-		}
-	})
-	
+			//Copy the xcore file from the tmp directory to services
+			//Ask Roxane about the #, her idea
+			var ouch = propertiesObject.artifactID+"/"+propertiesObject.artifactID+"/src/main/model/"
+			fs.createReadStream(propertiesObject.file.split('#')[0]).pipe(fs.createWriteStream(path.join(__dirname,"../services/",ouch+propertiesObject.file.split('#')[1])));
 
-	//Execute maven + send to maven repo ???
+
+			//zip the artifact
+			zipArchitecture("./"+propertiesObject.artifactID, function(err2,res){
+				if(err2){
+					console.log(err2); //Maybe do something else there (throw ?)
+				}else{
+					//return the artifact
+					callback(err2,res)//the .zip file
+				}
+			})
+			
+
+			//Execute maven + send to maven repo ???
+		}
+		
+	});
 }
 
 
