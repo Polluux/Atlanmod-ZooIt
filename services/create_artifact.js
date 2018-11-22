05d03ad5a040756747d4033f30b2b7d7bb34923a
Polluux <polluux88@yahoo.fr>
2018-11-22 14:03:24 +0100

    the pom.xml is modified to add the facultive properties specified in the form

--- services/create_artifact.js
+++ services/create_artifact.js
@@ -2,18 +2,19 @@ const fs = require('fs')
 const path = require('path')
 const zipFolder = require('zip-folder')
 const rimraf = require('rimraf')
+const xml2js = require('xml2js')
 
 const whitelist_regex = /^([A-Z]|[a-z]|[0-9]|\.|\-)+$/;
 
 
 function createFolder(dirPath,callback){
 	try{
-		fs.mkdirSync(path.join(__dirname,"../services/",dirPath))
+		fs.mkdirSync(path.join(__dirname,dirPath))
 		callback(null)
 	}catch(err){
 		if (err.code == "EEXIST"){
 			//Directory already exist, need to remove /!\
-			rimraf(path.join(__dirname,"../services/",dirPath,dirPath),function(){
+			rimraf(path.join(__dirname,dirPath,dirPath),function(){
 				callback(null)
 			})
 		}else{
@@ -36,7 +37,7 @@ function generateMavenArtifact(artifactID, groupID, version, callback){
 		var command = "yes |mvn archetype:generate -DarchetypeCatalog=local -DarchetypeGroupId=com.atlanmod.zoo -DarchetypeArtifactId=xcore-generation-archetype -DarchetypeVersion=1.0 -DgroupId="+groupID+" -DartifactId="+artifactID+" -Dversion="+version;
 		
 		const execSync = require('child_process').execSync;
-		execSync(command, {cwd: path.join(__dirname,"../services/",artifactID)}, (e, stdout, stderr)=> {
+		execSync(command, {cwd: path.join(__dirname,artifactID)}, (e, stdout, stderr)=> {
 			if (e instanceof Error) {	
 				console.error(e);	
 			}		
@@ -52,7 +53,7 @@ function generateMavenArtifact(artifactID, groupID, version, callback){
 
 
 function zipArchitecture(dirPath, callback){
-	zipFolder(path.join(__dirname,"../services/",dirPath),path.join(__dirname,"../services/",dirPath)+'.zip',function(err,res){
+	zipFolder(path.join(__dirname,dirPath),path.join(__dirname,dirPath)+'.zip',function(err,res){
 		if(err){
 			callback(err,null)
 		}else{
@@ -64,41 +65,104 @@ function zipArchitecture(dirPath, callback){
 }
 
 function createArtifact(propertiesObject,callback){
-	//create tmp folder with propertiesObject.artefact_name
-
 	console.log(propertiesObject)
 
+	//Check validity of the artifactID, to prevent a name such as "../artifact" which would cause issues
 	if(!whitelist_regex.test(propertiesObject.artifactID)){
-		//Yep, checked AGAIN !
 		callback("Error, you tried interring an unauthorized character...",null)
 	}else{
+
+		//Create a folder with propertiesObject.artefactID, used as a root folder for the zip
 		createFolder("./"+propertiesObject.artifactID,function(errFolder){
 			if(errFolder){
 				callback(errFolder,null)
 			}else{
-				generateMavenArtifact(propertiesObject.artifactID, propertiesObject.groupID, propertiesObject.version, function(err){
-			
+
+				//Generate the maven artifact (architecture + pom.xml)
+				generateMavenArtifact(propertiesObject.artifactID, propertiesObject.groupID, propertiesObject.version, function(err){			
 					if(err){
 						callback(err,null)
 					}else{
+
 						//Copy the xcore file from the tmp directory to services
-						//Ask Roxane about the #, her idea
-						var ouch = propertiesObject.artifactID+"/"+propertiesObject.artifactID+"/src/main/model/"
-						fs.createReadStream(propertiesObject.file.split('#')[0]).pipe(fs.createWriteStream(path.join(__dirname,"../services/",ouch+propertiesObject.file.split('#')[1])));
+						//Ask Roxane about the #, her idea (Split in 2 parts ->  [0] : The location of the file downloaded (in tmp) // [1] : The real name of the file)
+						var artifactArchitecturePath = propertiesObject.artifactID+"/"+propertiesObject.artifactID+"/src/main/model/"
+						fs.createReadStream(propertiesObject.file.split('#')[0]).pipe(fs.createWriteStream(path.join(__dirname,artifactArchitecturePath+propertiesObject.file.split('#')[1])));
 
 
-						//zip the artifact
-						zipArchitecture("./"+propertiesObject.artifactID, function(err2,res){
-							if(err2){
-								console.log(err2); //Maybe do something else there (throw ?)
+						//Modify the pom.xml to add facultatives properties
+						var parser = new xml2js.Parser()
+						var pomDir = path.join(__dirname,propertiesObject.artifactID+"/"+propertiesObject.artifactID+"/pom.xml")
+						fs.readFile(pomDir, function(readFileError, data) {
+							if(readFileError){
+								callback(readFileError,null)
 							}else{
-								//return the artifact
-								callback(err2,res)//the .zip file
-							}
-						})
-						
+								parser.parseString(data, function (xmlParseError, xmlParseResult) {
+									if(xmlParseError){
+										callback(xmlParseError, null)
+									}else{
+										console.log(xmlParseResult);
 
-						//Execute maven + send to maven repo ???
+								    	//Adding the facultatives properties specified in the form
+								    	if(propertiesObject.artifactName) xmlParseResult.project.name = propertiesObject.artifactName
+								    	if(propertiesObject.artefact_description) xmlParseResult.project.description = propertiesObject.artefact_description
+								    	if(propertiesObject.artifactURL) xmlParseResult.project.url = propertiesObject.artifactURL
+								    	if(propertiesObject.year) xmlParseResult.project.inceptionYear = propertiesObject.year
+								    	if(propertiesObject.organization) xmlParseResult.project.organization = { name: propertiesObject.organization }
+								    	var parseError = ""
+								    	if(propertiesObject.developpers){
+								    		xml2js.parseString(propertiesObject.developpers, function(devParseError,devParseResult){
+									    		if(devParseError){
+									    			parseError += "An error occured during parse of developers field :\n"+devParseError+"\n"
+									    		}else{
+									    			xmlParseResult.project.developers = devParseResult.developers
+									    		}
+									    	})
+								    	}
+								    	if(propertiesObject.scm){
+								    		xml2js.parseString(propertiesObject.scm, function(scmParseError,scmParseResult){
+									    		if(scmParseError){
+									    			parseError += "An error occured during parse of scm field :\n"+scmParseError+"\n"
+									    		}else{
+									    			xmlParseResult.project.scm = scmParseResult.scm
+									    		}
+									    	})
+								    	}
+
+								    	//NOTE : To avoid some more callbacks, we store eventual errors of parsing in the variable parseError
+								    	if(parseError !== ""){
+								    		callback(parseError,null)
+								    	}else{
+								    		
+								    		//Rebuild the xml file
+									    	var builder = new xml2js.Builder()
+									    	var xml = builder.buildObject(xmlParseResult)
+
+									    	//Rewrite the xml file
+									    	fs.writeFile(pomDir, xml, function(writeFileError, data){
+								            	if (writeFileError){
+								            		callback(writeFileError,null)
+								            	}else{
+
+								            		//Zip the artifact
+													zipArchitecture("./"+propertiesObject.artifactID, function(zipError,zipResult){
+														if(zipError){
+															console.log(zipError,null);
+														}else{
+															//return the artifact
+															callback(null,zipResult)//the .zip file
+														}
+													})
+													
+
+													//Execute maven + send to maven repo ???
+								            	}
+			        						})
+								    	}
+									}
+							    });
+							}
+						});
 					}
 				
 				});
